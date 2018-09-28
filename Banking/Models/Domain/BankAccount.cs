4dd1835fec58b4517e6643557179b95ca4b77c20
MathiasBaetens9160 <mathias.baetens.y1740@student.hogent.be>
2018-09-28 09:46:32 +0200

    Creëer klasse BankAccount

---
+++ Banking/Models/Domain/BankAccount.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Banking.Models.Domain {
+    public class BankAccount {
+        //private string _accountNumber;
+        #region Fields
+        //public const decimal WithdrawCost = 0.10M;
+        private decimal _balance; 
+        #endregion
+
+        #region Properties
+        public string AccountNumber {
+            get;
+            private set;
+        }
+
+        public decimal Balance {
+            get {
+                return _balance;
+            }
+            private set {
+                if (value < 0)
+                    throw new ArgumentException("No negative balance allowed");
+                _balance = value;
+            }
+        }
+        #endregion
+
+        #region Constructors
+
+        public BankAccount() {
+
+        }
+        public BankAccount(string accountNumber) {
+            AccountNumber = accountNumber;
+        }
+
+        public BankAccount(string accountNumber, decimal balance) : this(accountNumber) {
+            Balance = balance;
+        }
+        #endregion
+
+        //public decimal GetBalance() {
+        //    return _balance;
+        //}
+
+        //private void SetBalance(decimal value) {
+        //    _balance = value;
+        //}
+
+        //public decimal Balance { get; private set; }
+        
+
+        #region Methods
+        public void Deposit(decimal amount) {
+            Balance += amount;
+        }
+
+        public void Withdraw(decimal amount) {
+            Balance -= amount;
+        }
+        #endregion
+    }
+}
--- Banking/Program.cs
+++ Banking/Program.cs
@@ -1,12 +1,29 @@
-﻿using System;
+﻿using Banking.Models.Domain;
+using System;
 
 namespace Banking
 {
-    class Program
+    static class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            //Object Initializer
+            //BankAccount anotherBA = new BankAccount() {
+            //    AccountNumber = "123-12312334-99",
+            //    Balance = 200
+            //};
+
+            BankAccount myBA = new BankAccount("123-12312312-99", 50);
+
+            Console.WriteLine($"Account number is {myBA.AccountNumber}");
+            //Console.WriteLine($"Withdraw cost is {BankAccount.WithdrawCost} Euro");
+            Console.WriteLine($"Balance is {myBA.Balance} Euro");
+            myBA.Deposit(200);
+            Console.WriteLine($"Balance is {myBA.Balance} Euro");
+            myBA.Withdraw(50);
+            Console.WriteLine($"Balance is {myBA.Balance} Euro");
+            Console.ReadKey(); //Zodat console niet onmiddelijk afsluit
         }
     }
 }
